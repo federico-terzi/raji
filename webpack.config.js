fe33e12afeabfe4b6c36845a98ef1cb8c9a1db0a
Federico Terzi <federico-terzi@users.noreply.github.com>
2021-12-04 11:44:25 +0100

    change output format to umd+esm

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,59 @@
 const path = require("path");
 // const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.ts",
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    library: {
-      name: "raji",
-      type: "var",
+module.exports = [
+  {
+    entry: "./src/index.ts",
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
     },
-    filename: "raji.js",
-    path: path.resolve(__dirname, "dist"),
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+      library: {
+        name: "raji",
+        type: "umd",
+      },
+      filename: "raji.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    // optimization: {
+    //   minimize: false,
+    //   minimizer: [new TerserPlugin()],
+    // },
   },
-  // optimization: {
-  //   minimize: false,
-  //   minimizer: [new TerserPlugin()],
-  // },
-};
+  {
+    entry: "./src/index.ts",
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    experiments: {
+      outputModule: true,
+    },
+    output: {
+      library: {
+        type: "module",
+      },
+      filename: "raji.js",
+      path: path.resolve(__dirname, "esm"),
+    },
+  },
+];
